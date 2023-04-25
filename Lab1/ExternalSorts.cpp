bf8b80f848e214fa60b2b527394bfe72501223d8
Vlada Shudegova <vlada.shudegova@gmail.com>
2023-04-25 07:10:49 +0700

    Create a project Lab1 and add class ExternalSorts

---
+++ Lab1/ExternalSorts.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Vlada Shudegova on 25.04.2023.
+//
+
+#include "ExternalSorts.h"
---
+++ Lab1/ExternalSorts.h
@@ -0,0 +1,10 @@
+#ifndef LAB1_EXTERNALSORTS_H
+#define LAB1_EXTERNALSORTS_H
+
+
+class ExternalSorts {
+
+};
+
+
+#endif //LAB1_EXTERNALSORTS_H
