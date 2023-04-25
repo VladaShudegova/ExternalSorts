6353e4204006384895ff7c582f945e48e7834986
Vlada Shudegova <vlada.shudegova@gmail.com>
2023-04-25 11:04:55 +0700

    Finish code

--- Lab1/ExternalSorts.h
+++ Lab1/ExternalSorts.h
@@ -1,10 +1,18 @@
 #ifndef LAB1_EXTERNALSORTS_H
 #define LAB1_EXTERNALSORTS_H
-
+#include <fstream>
 
 class ExternalSorts {
 
+    void StraightMerge(const std::string& inputFileName);
+    void SplitFiles(const std::string& inputFileName, const std::string& outputFile1, const std::string& outputFile2);
+    bool CreateFileWithRandomNumbers(const std::string& fileName, const int numbersCount, const int maxNumberValue);
+    bool MergeFiles(const std::string& inputFile1, const std::string& inputFile2, const std::string& outputFile1, const std::string& outputFile2);
+
+
 };
+
+bool CreateFileWithRandomNumbers(const std::string& fileName, const int numbersCount, const int maxNumberValue);
 
 
 #endif //LAB1_EXTERNALSORTS_H
